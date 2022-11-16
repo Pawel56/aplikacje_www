68cd7648f8e2493dfcbeda128795aac8e5bc4c44
Paweł Janicki <pawel.jan526@gmail.com>
2022-11-16 13:25:09 +0100

    admin_listing i filtrowanie + naprawa migracji

--- mysite/zajecia/admin.py
+++ mysite/zajecia/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import person
+from .models import Person
+from .models import Team
 
-admin.site.register(person)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['imie', 'nazwisko', 'miesiac_urodzenia', 'data_dodania', 'druzyna']
+    list_filter = ('data_dodania', 'druzyna',)
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['nazwa', 'kraj']
+    list_filter = ('kraj',)
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Team, TeamAdmin)
+
--- mysite/zajecia/models.py
+++ mysite/zajecia/models.py
@@ -1,29 +1,40 @@
-from datetime import date
-
 from django.db import models
 
 
 # Create your models here.
-class person(models.Model):
-    MIESIACE = (
-        ('1', 'Styczeń'),
-        ('2', 'Luty'),
-        ('3', 'Marzec'),
-        ('4', 'Kwiecień'),
-        ('5', 'Maj'),
-        ('6', 'Czerwiec'),
-        ('7', 'Lipiec'),
-        ('8', 'Sierpień'),
-        ('9', 'Wrzesień'),
-        ('10', 'Październik'),
-        ('11', 'Listopad'),
-        ('12', 'Grudzień'),
-    )
+class Team(models.Model):
+   nazwa = models.CharField(max_length=200)
+   kraj = models.CharField(max_length=2)
+
+   def __str__(self):
+       return str(self.nazwa + " (" + self.kraj + ")")
+
+class Person(models.Model):
+    class Miesiace(models.IntegerChoices):
+        Styczen = 1
+        Luty = 2
+        Marzec = 3
+        Kwiecien = 4
+        Maj = 5
+        Czerwiec = 6
+        Lipiec = 7
+        Sierpien = 8
+        Wrzesien = 9
+        Pazdziernik = 10
+        Listopad = 11
+        Grudzien = 12
+
     imie = models.CharField(max_length=200)
     nazwisko = models.CharField(max_length=200)
-    miesiac_urodzenia = models.CharField(max_length=11, choices=MIESIACE, default=0)
+    miesiac_urodzenia = models.IntegerField(choices=Miesiace.choices, default=0)
     data_dodania = models.DateField(auto_now_add=True)
+    druzyna = models.ForeignKey(Team, default=None, on_delete=models.SET_DEFAULT, blank=True)
+
+    def __str__(self):
+        return str(self.imie+" "+self.nazwisko)
 
     class Meta:
         managed = True
         db_table = 'person'
+        ordering = ["nazwisko"]
+
