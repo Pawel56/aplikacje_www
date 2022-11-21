64228532970755b9f5c31e8fed0c0cbd5be988cf
Paweł Janicki <pawel.jan526@gmail.com>
2022-11-21 02:34:48 +0100

    dodanie serializeru dla Team + dodanie modelserializeru dla Person i Team

--- mysite/zajecia/serializers.py
+++ mysite/zajecia/serializers.py
@@ -16,3 +16,28 @@ class PersonSerializer(serializers.Serializer):
         instance.miesiac_urodzenia = validated_data.get('miesiac_urodzenia', instance.miesiac_urodzenia)
         instance.save()
         return instance
+
+class PersonModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ['id', 'name', 'miesiac_dodania', 'data_dodania', 'druzyna']
+        read_only_fields = ['id']
+
+class TeamSerializer(serializers.Serializer):
+    nazwa = serializers.CharField(required=True)
+    kraj = serializers.CharField(required=True)
+
+    def create(self, validated_data):
+        return Team.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.nazwa = validated_data.get('nazwa', instance.nazwa)
+        instance.kraj = validated_data.get('kraj', instance.kraj)
+        instance.save()
+        return instance
+
+class TeamModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ['id', 'nazwa', 'kraj']
+        read_only_fields = ['id']
